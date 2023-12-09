b265a4612bc8fcda45b491517ee0cc071a7e6e31
akshaypolisetty@gmail.com <you@example.com>
2023-12-09 11:34:23 +0000

    committ

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const initializeDBAndServer = async () => {
       filename: dbPath,
       driver: sqlite3.Database,
     });
-    app.listen(3001, () => {
+    app.listen(3000, () => {
       console.log("Server Running at http://localhost:3000/");
     });
   } catch (e) {
